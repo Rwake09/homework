e0d80d5b986fcf361fd5cd116b997f4bd68db7a5
Robert Wakefield <r.wakefield1987@gmail.com>
2023-06-15 18:18:37 +0100

    conditionals practice

---
+++ conditionals/conditionals.py
@@ -0,0 +1,13 @@
+a = 2
+b = 2
+
+
+# if statements
+
+# logical operators not equal !=
+if a == b:
+    print('a is equal to b')
+
+lang = ['Perl', 'python', 'PHP', 'Ruby']
+if 'python' in lang:
+    print('Python is found')
