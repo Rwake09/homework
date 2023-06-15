4c56aafac6f3fb824a46d07621489ea2d4766708
Robert Wakefield <r.wakefield1987@gmail.com>
2023-06-15 18:25:15 +0100

    conditionals practice

--- conditionals/conditionals.py
+++ conditionals/conditionals.py
@@ -11,3 +11,10 @@ if a == b:
 lang = ['Perl', 'python', 'PHP', 'Ruby']
 if 'python' in lang:
     print('Python is found')
+
+if a == b:
+    print('a is equal to b')
+elif a > b:
+    print('a is greater than b')
+else:
+    print('some other unexpected condition')
